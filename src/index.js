b276519aebcb40bacd2c43c438823380d3cfe809
onassau <85627253+onassau@users.noreply.github.com>
2021-06-15 14:53:05 +0200

    Use history to jump to given game state

--- src/index.js
+++ src/index.js
@@ -50,11 +50,14 @@ class Game extends React.Component {
         squares: Array(9).fill(null),
       }],
       xIsNext: true,
+      stepNumber: 0,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0,
+      this.state.stepNumber + 1);
+    
     const current = history[history.length - 1];
     const squares = current.squares.slice();
 
@@ -70,14 +73,32 @@ class Game extends React.Component {
         squares: squares,
       }]),
       xIsNext: !this.state.xIsNext,
+      stepNumber: history.length,
     });
   }
 
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: ((step % 2) === 0),
+    });
+  }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const label = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{label}</button>
+        </li>
+      );
+    });
 
     let status;
     if (winner) {
@@ -96,7 +117,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
