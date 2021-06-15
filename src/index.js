e93bb6a19c90940af79e4b5d5c633bf2d3ac42d9
onassau <85627253+onassau@users.noreply.github.com>
2021-06-15 13:46:46 +0200

    Add game ending clause

--- src/index.js
+++ src/index.js
@@ -21,6 +21,13 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+
+    // Do not change states when the game already has been won
+    // or when the given square is already filled
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
